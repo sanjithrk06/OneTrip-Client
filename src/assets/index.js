1ad676ad3888fcaf3fc8605d3368d0afe6f2884d
Harishsm20 <harishsm865@gmail.com>
2024-09-02 22:58:04 +0530

    dev: #11 Creating KindsOfDestinations

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,11 @@
 import HeroBg from './images/bg.jpg';
+import card1 from './images/Kd/k3.jpg'
+import card2 from './images/Kd/k4.jpg'
+import card3 from './images/Kd/k5.webp'
+import card4 from './images/Kd/k6.jpg'
+import card5 from './images/Kd/k2.jpg'
 
 export {
-    HeroBg
+    HeroBg,
+    card1,card2,card3,card4,card5
 }
--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ export { default as Footer } from './layout/Footer';
 
 // Sections Components
 export { default as Hero } from './section/Hero';
+export { default as KindOfDestination } from './section/kindOfDestination';
 
 // General Components
 export { default as GHero } from './general/Hero';

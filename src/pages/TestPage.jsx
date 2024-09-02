860df35fa394c163dffc8e217b3fd58dc3955e10
sanjithrk06 <sanjithkarthikeyanr@gmail.com>
2024-09-02 16:41:39 +0530

    test commit

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,7 +1,12 @@
+// Components import 
+import { GHero } from "../components";
 
 const TestPage = () => {
   return (
+    <>
+    <GHero header={"Destinations"} desc={"test description"} />
     <div className=' h-screen pt-20'>TestPage</div>
+    </>
   )
 }
 

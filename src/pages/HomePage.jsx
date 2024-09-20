1fa1100202a2f1f346e6f60328e2006cd906fee7
Harishsm20 <harishsm865@gmail.com>
2024-09-20 13:54:33 +0530

    Template for Each Destination Page Completed #14

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 // Page Components import
 import { Hero } from '../components';
 
+// Hero Section Bg
+import { HeroBg } from '../assets';
+
 const HomePage = () => {
   return (
     <>
-    <Hero />
+    <Hero img={HeroBg}/>
     </>
   )
 }

834e6fde2bfaacb97ff75c2bdbfa7ac7529dff0f
Harishsm20 <harishsm865@gmail.com>
2025-01-04 14:44:56 +0530

    Test commit

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
   return (
     <nav
       className={`fixed top-0 w-full z-50 py-3  ${
-        isScrolled ? "bg-gray-600 backdrop-blur-3xl" : "bg-orange-500/90"
+        isScrolled ? "bg-gray-600 backdrop-blur-3xl" : "bg-gray-100/10 backdrop-blur-3xl"
       } transition-all duration-300`}
     >
       <div className="container py-1 px-4 mx-auto relative lg:text-sm">
